4c6b6147e23c70bc76c1d0c7703596149e39a47a
Nekoayah <133284929+AliyahAira@users.noreply.github.com>
2025-04-16 06:01:10 +0000

    Lab SW6 animal prototype

--- TestAnimal.java
+++ TestAnimal.java
@@ -2,16 +2,16 @@ public class TestAnimal {
     public static void main(String[] args) {
         AnimalRegistry registry = new AnimalRegistry();
 
-        Animal dolly = registry.createSheep("Imee");
-        Animal bessie = registry.createCow();
-        Animal spirit = registry.createHorse();
+        Animal imee = registry.createSheep("Imee");
+        Animal john = registry.createCow();
+        Animal barbie = registry.createHorse();
 
-        dolly.makeSound();
-        bessie.makeSound();
-        spirit.makeSound();
+        imee.makeSound();
+        john.makeSound();
+        barbie.makeSound();
 
-        System.out.println(dolly);
-        System.out.println(bessie);
-        System.out.println(spirit);
+        System.out.println(imee);
+        System.out.println(john);
+        System.out.println(barbie);
     }
 }
